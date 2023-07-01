282727ce23af11b36c0cbf932f2be44009338e19
lrojas <lrojas@credisoluciones.com>
2023-07-01 10:17:06 -0600

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+import { publicCanActivateGuard, publicCanMatchGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+    canActivate: [publicCanActivateGuard],
+    canMatch: [publicCanMatchGuard]
   },
   {
     path: 'albums',
-    loadChildren: () => import('./albums/heroes.module').then( m => m.AlbumsModule ),
-    canActivate: [ canActivateGuard ],
-    canMatch: [ canMatchGuard ]
+    loadChildren: () =>
+    import('./albums/heroes.module').then((m) => m.AlbumsModule),
+    canActivate: [canActivateGuard], //Anclamos la función del canActive
+    canMatch: [canMatchGuard], //Anclamos la función del canMatch
   },
   {
     path: '404',
--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -8,35 +8,31 @@ import {
     UrlSegment,
   } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { inject } from '@angular/core';
    
   const checkAuthStatus = (): boolean | Observable<boolean> => {
-    //se inyectan el AuthService y el Router
     const authService: AuthService = inject(AuthService);
     const router: Router = inject(Router);
    
     return authService.checkAuthentication().pipe(
       tap((isAuthenticated) => {
-        if (!isAuthenticated) {
-          router.navigate(['/auth/login']);
+        if (isAuthenticated) {
+          router.navigate(['./']);
         }
-      })
+      }),
+      map( isAuthenticated => !isAuthenticated)
     );
   };
    
-  //No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
-  export const canActivateGuard: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
+  export const publicCanActivateGuard: CanActivateFn = ( 
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ) => {
-    console.log('CanActivate');
-    console.log({ route, state });
-   
+  ) => {   
     return checkAuthStatus();
   };
    
-  export const canMatchGuard: CanMatchFn = ( //Tipado CanMatchFN
+  export const publicCanMatchGuard: CanMatchFn = ( 
     route: Route,
     segments: UrlSegment[]
   ) => {
